313a90fd2064b0128ac5933c5cc2f81bf8371a39
Akhil Ankireddy <akhil.vedas@outlook.com>
2021-02-04 18:26:55 +0530

    Implement Login With Employee Id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_params
     added_attrs = %i[first_name last_name username email password password_confirmation remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,30 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :username, presence: true, length: { minimum: 3 }
+  validates :username, presence: true, length: { minimum: 3 }, uniqueness: { case_sensitive: false }
   validates :first_name, presence: true, length: { minimum: 2 }
   validates :last_name, presence: true, length: { minimum: 2 }
 
   has_many :challenges, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :collaborations, dependent: :destroy
+
+  attr_writer :login
+
+  def login
+    @login || id || username || email
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(concat('E', id)) = :value OR lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+  end
 end
